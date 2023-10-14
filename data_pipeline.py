c19832711f6dafd6ebb11df6f8f6b3143cd663cb
felix-ha <hauerf98@gmail.com>
2023-10-14 22:20:30 +0200

    Return result from pipeline and test with dependency

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,4 +1,6 @@
 import time
+from dataclasses import dataclass
+import torch
 
 from tokenizer import (
     split_tokens_raw,
@@ -6,6 +8,7 @@ from tokenizer import (
     get_unique_tokens,
     create_token_to_int_dicts,
     create_encoder,
+    create_decoder,
 )
 from data_prep import split_corpus, texts_to_input_ids, input_ids_to_tensor
 from constants import *
@@ -23,6 +26,33 @@ def replace_characters(corpus, replacment_dict):
     return corpus
 
 
+@dataclass
+class Data:
+    """
+    Result from data pipeline that builds a data set for a language model.
+
+    Args:
+        n_positions: Number of positions in the input tensor.
+        vocab_size: Size of the vocabulary.
+        token_to_int: Mapping from tokens to integers.
+        encoder: Callable that converts a string to a list of tokens (int).
+        int_to_token: Mapping from integers to tokens.
+        decoder: Callable that converts a list of integers to a string.
+        X_train: Input tensor for training.
+        X_validation: Input tensor for validation.
+
+    """
+
+    n_positions: int
+    vocab_size: int
+    token_to_int: dict
+    encoder: callable
+    int_to_token: dict
+    decoder: callable
+    X_train: torch.tensor
+    X_validation: torch.tensor
+
+
 def pipeline(file_path_train, file_path_validation):
     start_time = time.perf_counter()
 
@@ -35,6 +65,7 @@ def pipeline(file_path_train, file_path_validation):
     tokens_unique = get_unique_tokens(tokens_all)
     token_to_int, int_to_token = create_token_to_int_dicts(tokens_unique)
     encoder = create_encoder(token_to_int, DELIMTERS, TOKEN_TO_REMOVE)
+    decoder = create_decoder(int_to_token)
 
     print_unique_characters(corpus_train_raw)
     print(f"Size of vocabulary: {len(int_to_token)}")
@@ -56,4 +87,13 @@ def pipeline(file_path_train, file_path_validation):
     X_train = input_ids_to_tensor(texts_ids_train)
     X_validation = input_ids_to_tensor(texts_ids_validation)
 
-    assert True
+    return Data(
+        n_positions=X_train.shape[1],
+        vocab_size=len(int_to_token),
+        token_to_int=token_to_int,
+        encoder=encoder,
+        int_to_token=int_to_token,
+        decoder=decoder,
+        X_train=X_train,
+        X_validation=X_validation,
+    )
--- test_data_pipeline.py
+++ test_data_pipeline.py
@@ -1,6 +1,39 @@
+"""
+This module contains a unit tests for the complete data pipeline.
+As a final test, the output will be fed into the transformer.
+"""
+
+import pytest
 from data_pipeline import pipeline
+from transformers import GPT2Config, GPT2LMHeadModel
 
 
-def test_pipeline():
-    pipeline("data/data_train.txt", "data/data_validation.txt")
-    assert True
+@pytest.fixture
+def data():
+    value = pipeline("data/data_train.txt", "data/data_validation.txt")
+    yield value
+
+
+@pytest.mark.dependency()
+def test_data_pipeline(data):
+    assert data.n_positions == 0
+
+
+@pytest.mark.dependency(depends=["test_data_pipeline"])
+def test_training(data):
+    gpt2_config = GPT2Config(
+        n_positions=data.n_positions,
+        vocab_size=data.vocab_size,
+        n_embd=5,
+        n_inner=5,
+        n_layer=1,
+        n_head=1,
+    )
+
+    model = GPT2LMHeadModel(gpt2_config)
+
+    x = data.X_validation
+    batch_size, context = x.shape
+
+    y_logits = model(x).logits
+    assert y_logits.shape == (batch_size, context, data.vocab_size)
