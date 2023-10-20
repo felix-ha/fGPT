14dea52901f9c1efc5a34823d18f7752acefdf1a
felix-ha <hauerf98@gmail.com>
2023-10-20 17:50:23 +0200

    Move code to main

--- data_pipeline.py
+++ data_pipeline.py
@@ -4,9 +4,6 @@ import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
-from dionysus.training import TrainingConfig, train
-from model import simpleGPT, cross_entropy_language_model, generate
-
 from tokenizer import (
     split_tokens_raw,
     clean_tokens,
@@ -125,53 +122,3 @@ def pipeline(file_path_train, file_path_validation):
         dataloader_train=dataloader_train,
         dataloader_validation=dataloader_validation,
     )
-
-
-if __name__ == "__main__":
-    data = pipeline("data/data_train.txt", "data/data_validation.txt")
-
-    stop_token_id = data.token_to_int[END_OF_TEXT]
-
-    model = simpleGPT(
-        data.vocab_size,
-        n_embd=8,
-        num_heads=4,
-        block_size=data.n_positions,
-        n_layer=1,
-        dropout=0.1,
-        device="cpu",
-    )
-
-    loss_func = cross_entropy_language_model
-
-    train_config = TrainingConfig(
-        model=model,
-        epochs=2,
-        loss_func=loss_func,
-        training_loader=data.dataloader_train,
-        validation_loader=data.dataloader_validation,
-        optimizer="AdamW",
-        device="gpu",
-        save_model=True,
-        tar_result=True,
-        save_path="runs",
-        model_name="GPT-2",
-        progress_bar=True,
-    )
-
-    train(train_config)
-
-    prompt = "Tom was"
-    output, choices = generate(
-        model,
-        prompt,
-        data.encoder,
-        data.decoder,
-        stop_token_id=stop_token_id,
-        max_n=5,
-        choices_per_step=3,
-    )
-
-    logging.info(f"\n{choices}")
-    logging.info(f"Promt: {prompt}")
-    logging.info(f"Model: {output}")
--- main.py
+++ main.py
@@ -1,6 +1,56 @@
 import logging
 logging.basicConfig(format="%(asctime)s %(message)s", level=logging.INFO)
 
+from dionysus.training import TrainingConfig, train
+from model import simpleGPT, cross_entropy_language_model, generate
+from data_pipeline import pipline
+from constants import *
+
 if __name__ == "__main__":
-    logging.info("hello")
-    
+    data = pipeline("data/data_train.txt", "data/data_validation.txt")
+
+    stop_token_id = data.token_to_int[END_OF_TEXT]
+
+    model = simpleGPT(
+        data.vocab_size,
+        n_embd=8,
+        num_heads=4,
+        block_size=data.n_positions,
+        n_layer=1,
+        dropout=0.1,
+        device="cpu",
+    )
+
+    loss_func = cross_entropy_language_model
+
+    train_config = TrainingConfig(
+        model=model,
+        epochs=2,
+        loss_func=loss_func,
+        training_loader=data.dataloader_train,
+        validation_loader=data.dataloader_validation,
+        optimizer="AdamW",
+        device="gpu",
+        save_model=True,
+        tar_result=True,
+        save_path="runs",
+        model_name="GPT-2",
+        progress_bar=True,
+    )
+
+    train(train_config)
+
+    prompt = "Tom was"
+    output, choices = generate(
+        model,
+        prompt,
+        data.encoder,
+        data.decoder,
+        stop_token_id=stop_token_id,
+        max_n=5,
+        choices_per_step=3,
+    )
+
+    logging.info(f"\n{choices}")
+    logging.info(f"Promt: {prompt}")
+    logging.info(f"Model: {output}")
